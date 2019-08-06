2340479361db406da2213c1c4667e1e00a9f4b5e
chenjun <897221533@qq.com>
2019-08-06 14:38:27 +0800

    新增镜像文件

---
+++ push-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#push google or coreos images to dockerhub or aliyun registry,
+#change the registry name and username/password to yourself's.
+
+ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/aliwill
+ALI_USERNAME=2008chen.jun@gmail.com
+ALI_PASSWORD=871002/*-cj
+
+#config the following env at travis-ui
+docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+
+for image in $(cat img-list.txt)
+do
+	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	docker pull $image
+	docker tag $image $ALI_REGISTRY/$imagename
+	# push到阿里云仓库
+	docker push $ALI_REGISTRY/$imagename
+done
